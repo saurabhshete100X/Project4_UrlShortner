9da0d1b8e89b188a8fde5652c84c95083117cf18
NehaVerma009 <nehuverma009@gmail.com>
2022-09-30 13:01:32 +0530

    Validation

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,92 +1,98 @@
-const validUrl = require('valid-url')
-const shortid = require('shortid')
+const validUrl = require("valid-url");
+const shortid = require("shortid");
 //const isValid = require('../validations/validation')
-const urlModel = require('../models/urlModel')
+const urlModel = require("../models/urlModel");
 //const Url = require('../models/urlModel')
 
-const baseUrl = 'http://localhost:3000/'
+const baseUrl = "http://localhost:3000/";
 
-const urlRegex = (value) => {                                  //  USING THIS REGEX TO VALIDATE URL PATTERN
-    let urlRegex = /^(?:(?:(?:https?|http):)?\/\/.*\.(?:png|gif|webp|com|in|org|co|co.in|net|jpeg|jpg))/i;
-    if (urlRegex.test(value))
-        return true;
-}
+const urlRegex = (value) => {
+  //  USING THIS REGEX TO VALIDATE URL PATTERN
+  let urlRegex =
+    /^(?:(?:(?:https?|http):)?\/\/.*\.(?:png|gif|webp|com|in|org|co|co.in|net|jpeg|jpg))/i;
+  if (urlRegex.test(value)) return true;
+};
 
-const createUrl = async function(req, res){
-    try{
-let data = req.body
+const createUrl = async function (req, res) {
+  try {
+    let data = req.body;
 
-if (Object.keys(data).length == 0) {
-    return res
-      .status(400)
-      .send({ status: false, message: "Body should not be empty" });
+    if (Object.keys(data).length == 0) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Body should not be empty" });
+    }
+
+    const keys = ["longUrl"];
+
+    if (!Object.keys(req.body).every((elem) => keys.includes(elem))) {
+      return res
+        .status(400)
+        .send({ status: false, message: "wrong Parameters" });
+    }
+
+
+    const isValid= function (value) {
+        if (typeof value === 'undefined' || value === null) return false
+        if (typeof value === 'string' && value.trim().length === 0) return false
+        return true
+    }
+
+     if(!isValid(data.longUrl)){
+        return res.status(400).send({status: false, message: "Data should be in string"})
+     }
+
+
+    if (!urlRegex(data.longUrl))
+      return res.status(400).send({ status: false, message: "Wrong Url" });
+
+    
+    // let validurl = isValid.isValidUrl(data)
+    //     if (validurl) {
+    //         return res.status(400).send({ status: false, message: validurl})
+    //       }
+
+    //"http://localhost:3000/"
+    let permanentUrl = await urlModel
+      .findOne({ longUrl: data.longUrl })
+      .select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 0 });
+
+    if (!permanentUrl) {
+      let urlCode = shortid.generate().toLocaleLowerCase();
+      let shortUrl = baseUrl + urlCode;
+      data.urlCode = urlCode;
+      data.shortUrl = shortUrl;
+      await urlModel.create(data);
+      res.status(201).send({ status: true, data: data });
+    } else {
+      return res
+        .status(200)
+        .send({ status: true, message: "Already generated", data: permanentUrl });
+    }
+  } catch (err) {
+    res.status(500).send({ status: false, message: err.message });
   }
+};
 
-  const keys = ["longUrl"];
-
-  if (!Object.keys(req.body).every((elem) => keys.includes(elem))) {
-    return res.status(400).send({ status: false, message: "wrong Parameters" });
-  }
-
-  if(!urlRegex(data.longUrl))  return res.status(400).send({status:false,message:"Wrong Url"})
- 
-//   let regex = (/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(data))
-//   if (!regex){
-//     return res.status(400).send({status:false,message:"Invalid URL"})
-//   }
-// let validurl = isValid.isValidUrl(data)
-//     if (validurl) {
-//         return res.status(400).send({ status: false, message: validurl})
-//       }
-
-
-//"http://localhost:3000/"
-
-let urlCode = shortid.generate().toLocaleLowerCase()
-let shortUrl = baseUrl+urlCode
-data.urlCode= urlCode
-data.shortUrl = shortUrl
-await urlModel.create(data)
-res.status(201).send({status:true,data:data})
+const getUrl = async function (req, res) {
+  try {
+    // find a document match to the code in req.params.code
+    const url = await urlModel.findOne({
+      urlCode: req.params.urlCode,
+    });
+    if (url) {
+      // when valid we perform a redirect
+      return res.redirect(url.longUrl);
+    } else {
+      // else return a not found 404 status
+      return res.status(404).send({ status: false, message: "No URL Found" });
     }
-    catch(err){
-        res.status(500).send({status:false,message:err.message})
-    }
-
-
-}
-
-
-
- const getUrl= async function(req,res){
-    try {
-        // find a document match to the code in req.params.code
-        const url = await urlModel.findOne({
-            urlCode: req.params.urlCode
-        })
-        if (url) {
-            // when valid we perform a redirect
-            return res.redirect(url.longUrl)
-        } 
-        else {
-            // else return a not found 404 status
-            return res.status(404).send({status: false,message:'No URL Found'})
-        }
-
-    }
+  } catch (err) {
     // exception handler
-    catch (err) {
-        console.error(err)
-        res.status(500).send({status: false,message:'Server Error'})
-    }
-}
- 
+    console.error(err);
+    res.status(500).send({ status: false, message: "Server Error" });
+  }
+};
 
-
-
-
-
-
-
-module.exports.createUrl= createUrl
-module.exports.getUrl= getUrl
+module.exports.createUrl = createUrl;
+module.exports.getUrl = getUrl;
--- src/validations/validation.js
+++ src/validations/validation.js
@@ -1,17 +1,14 @@
 
 
-function  validRegex(url){
-    return /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(url)
-}
+// function  validRegex(url){
+//     return /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(url)
+// }
 
 
-const isValid= function (value) {
-    if (typeof value === 'undefined' || value === null) return false
-    if (typeof value === 'string' && value.trim().length === 0) return false
-    return true
-}
+
 
 const isValidUrl = function (url) {  
+
     try {
 if(!url){
     return "Please Inpur URl"
@@ -21,15 +18,10 @@ if(!isValid(url)){
     return "url invalid"
 }
 
-
-if(!validRegex(url)){
-    return "Invalid URL"
-}
     }
     catch(err){
         return res.status(500).send(err.message)
     }
 }
-
 
 module.exports.isValidUrl= isValidUrl
